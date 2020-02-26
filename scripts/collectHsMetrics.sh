bc37b89b0ec1dabd54a16207487f88348f1ade71
sormond <38035589+sormond@users.noreply.github.com>
2020-02-26 14:06:07 +1300

    Update collectHsMetrics.sh

--- scripts/collectHsMetrics.sh
+++ scripts/collectHsMetrics.sh
@@ -4,6 +4,7 @@
 
 ## Variables
 # reference.fasta=genome reference fasta (hg38 for all alignments in this project)
+# path=file path to picard programme
 # WES_kit_target.bed=WES kit target region bed
 # WES_target_interval=Bedtools output interval file containing WES kit target regions
 # reference_genome.dict=.dict file for genome reference created by samtools faidx
@@ -14,7 +15,7 @@
 samtools faidx reference.fasta
 
 ## picard BedToIntervalList for creation of picard-compatible interval files
-java -jar ~/picard.jar BedToIntervalList I=WES_kit_target.bed O=$WES_target_interval SD=$reference_genome.dict ~/MH/reference/GATK_Bundle/bwa/Homo_sapiens_assembly38.dict
+java -jar ${path}picard.jar BedToIntervalList I=WES_kit_target.bed O=$WES_target_interval SD=$reference_genome.dict ~/MH/reference/GATK_Bundle/bwa/Homo_sapiens_assembly38.dict
 
 ## picard CollectHsMetrics for coverage measurements
-java -jar ~/picard.jar CollectHsMetrics I=$sample.bam O=$sample_output_hs_metrics.txt R=$reference.fasta BAIT_INTERVALS=$WES_target_interval TARGET_INTERVALS=$WES_target_interval
+java -jar ${path}picard.jar CollectHsMetrics I=$sample.bam O=$sample_output_hs_metrics.txt R=$reference.fasta BAIT_INTERVALS=$WES_target_interval TARGET_INTERVALS=$WES_target_interval
