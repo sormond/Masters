4ef8014814febab43cec8a2d56cbcaebf8555867
sormond <38035589+sormond@users.noreply.github.com>
2020-04-30 21:31:12 +0200

    Update variant_calling_gatk.sh

--- scripts/variant_calling_gatk.sh
+++ scripts/variant_calling_gatk.sh
@@ -7,12 +7,6 @@
 # reference.fasta=genome reference fasta (hg38 for all alignments in this project)
 # processedbam=bam file to be used
 # output.g.vcf.gz=output gzipped gvcf
-# 
-# 
-# 
-#
-# 
-#
 
 # gatk HaplotypeCaller
 gatk --java-options "-Xmx4g" HaplotypeCaller --native-pair-hmm-threads $INT -R $reference.fasta -I $processedbam -O $output.g.vcf.gz -ERC GVCF
